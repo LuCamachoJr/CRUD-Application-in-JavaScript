02b45921dff2d12a3af5e8c39b740261d398e9ce
Luis Camacho Jr <LuisCamachoJr@fullsail.edu>
2013-10-01 21:06:15 -0400

    Updated

--- js/asd-1308-jQuery.js
+++ js/asd-1308-jQuery.js
@@ -2,7 +2,7 @@
  * Author: Luis Camacho Jr.
  * Full Sail University
  * Course: ASD
- * Term: 1308
+ * Term: 1310
  * Project Week 2
  * Reference js from previous courses
  * refactor functions using jquery
