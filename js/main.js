da8ef3b68ef1d537ec59ea6b4161a177dc64cdfa
Luis Camacho Jr <LuisCamachoJr@fullsail.edu>
2013-10-03 11:31:51 -0400

    Project Architecture week 1 code

--- js/main.js
+++ js/main.js
@@ -3,5 +3,39 @@
  * Full Sail University
  * Course: ASD
  * Term: 1310
- * boilerplate Activity Week 1
+ * Architecture Code Week 1
  */
+
+
+$('#pageOne').on('pageinit', function(){
+
+    //code needed for home page goes here
+});
+
+
+$('#addProject').on('pageinit', function(){
+
+    //code needed for Form page goes here
+});
+
+$('#viewProjects').on('pageinit', function(){
+
+    //code needed for view project page goes here
+});
+
+
+$('#viewIdeas').on('pageinit', function(){
+
+    //code needed for view idea page goes here
+});
+
+
+$('#viewCompleted').on('pageinit', function(){
+
+    //code needed for view completed page goes here
+});
+
+
+
+
+
